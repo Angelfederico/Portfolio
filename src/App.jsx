6d0b3839ff27f0baf5fd25b35d7441521ff5c091
Angel Federico <121796085+Angelfederico@users.noreply.github.com>
2023-02-04 20:55:55 +0000

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>Portfolio principal</h1>
-
+<h2>Este es u</h2>
     </>
   )
 }
